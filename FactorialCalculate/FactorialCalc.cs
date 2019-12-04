97c63dc2a7fe7e7acc6d05841d6315aad3b6518c
esim7 <makishev@yahoo.com>
2019-12-05 00:09:36 +0600

    Загрузил задание

---
+++ FactorialCalculate/FactorialCalc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactorialCalculate
+{
+    public class FactorialCalc
+    {
+        public int Calculate(List<string> args)
+        {
+            var result = 0;
+            for (int i = 0; i < args.Count; i++)
+            {
+                result += int.Parse(args[i]);
+            }
+            return result;
+        }
+    }
+}
---
+++ FactorialCalculate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FactorialCalculate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = new List<string>();
+            numbers = args.ToList();
+        }
+        public static string Factorial(List<string> args)
+        {
+            ulong result = 0;
+            for (int j = 0; j < args.Count; j++)
+            {
+                ulong soloFactorial = 1;
+                for (int i = 1; i <= int.Parse(args.ElementAt(j)); i++)
+                {
+                    soloFactorial *= (ulong)i;
+                }
+                result += soloFactorial;
+            }
+            return result.ToString();
+        }
+    }
+}
